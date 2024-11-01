5eba48a5bddb1e304f6661e596d2b747e1610971
suzuka_kishimoto <suzuka.kishimoto05@gmail.com>
2024-11-01 16:52:53 +0900

    feat: ヘッダーの作成

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,6 +1,12 @@
+import React, {Fragment} from 'react'
+import './index.css'; // CSSファイルの正しいパスを指定
+import Logo from './assets/header-logo.png'
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
+    <div id="header">
+      <img src={Logo} alt="Logo"/>
+      <h1>React Investment Calculator</h1>
+    </div>
   )
 }
 
